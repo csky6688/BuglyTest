2bd164125289c99fb5a6b1113eefec7a30ee2c08
csky6688 <qz600656>
2016-01-11 14:55:53 +0800

    2.0
    测试

--- app/src/main/java/example/com/buglytest/MyApplication.java
+++ app/src/main/java/example/com/buglytest/MyApplication.java
@@ -1,12 +1,12 @@
 package example.com.buglytest;
 
-import com.tencent.bugly.crashreport.CrashReport;
+//import com.tencent.bugly.crashreport.CrashReport;
 
 /**
  * Created by hzfengqizhong on 2015/12/8.
  */
 public class MyApplication {
     static {
-        CrashReport.initCrashReport(null, "900013823", false);
+       // CrashReport.initCrashReport(null, "900013823", false);
     }
 }
